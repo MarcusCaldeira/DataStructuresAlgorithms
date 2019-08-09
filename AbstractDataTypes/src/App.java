21534feeca8f27f6e8d603f2ff4a6a5b02484d66
MarcusCaldeira <mac.caldr@gmail.com>
2019-08-09 10:49:36 -0500

    Learned how to create a main method in Java. PSVM-TAB: this will generate a main method for the class your working on. WOOT.

--- AbstractDataTypes/src/App.java
+++ AbstractDataTypes/src/App.java
@@ -2,6 +2,10 @@ public class App {
 
     //psvm-TAB: will give you a new main method.
     public static void main(String[] args) {
+        Counter myCounter = new Counter("myCounter");
+        myCounter.increment();
+        myCounter.increment();
+        myCounter.increment();
 
     }
 
