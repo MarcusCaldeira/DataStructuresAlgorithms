00b66c526697fa063467648bf0f665c875a226e0
MarcusCaldeira <mac.caldr@gmail.com>
2019-08-20 12:47:36 -0500

    looking ath what makes this an actual stack, digging into what makes pop add onto the stack.

--- StackDataStructure/src/Stack.java
+++ StackDataStructure/src/Stack.java
@@ -4,4 +4,22 @@ public class Stack {
     private long [] stackArray;
     //will represent the last item placed on top of the stack.
     private int top;
+
+
+    public Stack(int Size) {
+        this.maxSize = Size;
+        this.stackArray = new long[maxSize];
+        this.top = -1;
+    }
+    public void push(long j){
+        top++;
+        stackArray[top] = j;
+    }
+    public long pop(){
+        int oldTop = top;
+        top--;
+        return stackArray[oldTop];
+    }
+
+
 }
